b704b9bd0b36619950efb2cdbbe3b110c2ec2d4d
Noel Aczel <ken.aczel@gmail.com>
2024-12-08 07:01:08 +0000

    KUCSS Build path url

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
+      base: process.argv.includes("dev") ? "" : "/KUCSS",
     },
     alias: {
       "@components": path.resolve("src/lib/components"),

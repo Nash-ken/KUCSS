0001e9671aed584ef051119228480e4669b5ba5a
Noel Aczel <ken.aczel@gmail.com>
2024-12-08 03:49:48 +0000

    applied some changes for load function

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ import path from "path";
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess({ script: true }),
 
   kit: {
     adapter: adapter({

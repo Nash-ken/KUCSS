75897d0d8d7e41b66e0a60540d96640aeea6187a
Noel Aczel <ken.aczel@gmail.com>
2024-12-08 06:54:50 +0000

    Events and pages setup

--- src/lib/components/Layout/Navigation.svelte
+++ src/lib/components/Layout/Navigation.svelte
@@ -1,1 +1,49 @@
-<nav></nav>
+<script lang="ts">
+    import { page } from "$app/stores";
+    import Icon from "@iconify/svelte";
+
+    let currentPath: string = $derived($page.url.pathname);
+
+    const pages = [
+        {label: "Home", link: '/'},
+        {label: "Events", link: '/Events'},
+        {label: "Learning", link: '/Learning'},
+        {label: "About", link: '/About'},
+
+    ]
+
+    function isActive(page: string): boolean {
+        if(currentPath === page) {
+            return true
+        }
+        return false
+    }
+</script>
+
+<nav>
+    <div class="flex items-center text-xl font-bold font-geist">
+        <span class="text-text-muted text-lg">./</span>
+        <span>KUCSS</span>
+    </div>
+      
+    <ul class="desktop:flex ml-6 space-x-6 items-center hidden">
+       {#each pages as anchor}
+            <a href={anchor.link} class="font-semibold" class:active={isActive(anchor.link)}>{anchor.label}</a>
+       {/each}
+    </ul>
+
+    <button class="ml-auto desktop:hidden flex">
+        <Icon icon="heroicons-outline:menu-alt-4" width="32" height="32" />
+    </button>
+</nav>
+
+
+<style lang="postcss">
+    a.active {
+        @apply text-text;
+    }
+
+    a:not(.active) {
+        @apply text-text-muted;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,47 @@
 <script lang="ts">
     import { base } from "$app/paths";
-  import { onMount } from "svelte";
-  import type { PageData } from "./$types";
-  let { data }: { data: PageData } = $props();
-  type event = {
-    name: string
-  };
+    import Icon from "@iconify/svelte";
+    import type { PageData } from "./$types";
+    let { data }: { data: PageData } = $props();
+    type event = {
+        name: string
+    };
     
 </script>
 
-<h1 class="desktop:text-6xl text-5xl font-bold mt-48">Let's build from here</h1>
+<h1 class="desktop:text-6xl text-5xl font-bold mt-64">Let's build from here</h1>
 <p class="mt-3 font-medium text-xl">Connecting Cyber Security Enthusiasts and Professionals</p>
 
 <div class="flex flex-wrap mt-6 space-y-4 desktop:space-y-0">
-    <a href={base} class="font-medium px-6 py-3 rounded-full border-border border w-full desktop:text-start text-center desktop:w-fit hover:ring ring-border">Discord</a>
+    <a href={base} class="font-medium px-6 py-3 rounded-full border-border border w-full desktop:text-start text-center desktop:w-fit hover:ring ring-border flex items-center gap-2 desktop:gap-3 desktop:justify-start justify-center">
+        <Icon icon="simple-icons:discord" width="24" height="24" />
+        Discord
+    </a>
     <a href="" class="font-medium px-6 py-3 text-center w-full desktop:w-fit hover:underline">Kingston University &rightarrow;</a>
 </div>
 
-<div class="flex w-full mt-12">
-    <div class="rounded-xl border border-border p-6 w-full desktop:max-w-96 flex flex-col">
+<div class="flex flex-col desktop:flex-row w-full mt-12">
+    <div class="rounded-xl border border-border p-6 w-full desktop:max-w-96  flex flex-col">
         <h3 class="text-2xl font-medium">Upcoming Events</h3>
         <p class="mt-4 text-text-muted">Join us for our Events covering all things security.</p>
-            {#each data.events as event}
-                <p>{event.name}</p>
-            {/each}
+        <div class="flex-1 min-h-48 bg-primary my-3 p-4 rounded-xl">
+            wack
+        </div>
+        <button class="mt-auto px-6 py-4 bg-primary text-text-muted text-sm font-semibold hover:ring ring-border rounded-full">View All Events</button>
+    </div>
+
+    <div class="flex flex-col desktop:mt-0 desktop:ml-12 mt-12 ml-0 space-y-4 w-full max-w-96">
+        <div class="flex flex-col w-full h-32">
+            <h1 class="font-bold text-4xl">300+</h1>
+            <p class="text-text-muted font-bold text-lg">Students learning and potential connections</p>
+        </div>
+        <div class="flex flex-col w-full h-32">
+            <h1 class="font-bold text-4xl">10x</h1>
+            <p class="text-text-muted font-bold text-lg">Your productivity with projects to put on your resume</p>
+        </div>
+        <div class="flex flex-col w-full h-32">
+            <h1 class="font-bold text-4xl">5x</h1>
+            <p class="text-text-muted font-bold text-lg">More Technical skills acquired</p>
+        </div>
     </div>
 </div>
